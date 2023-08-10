39afdb447e493eaa486c8ccedaf61ef1b2d51f30
Chia Hung "Simon" Lee (he/him) <simonlee0423@gmail.com>
2023-08-10 15:32:43 -0400

    Camera Disables when Tilemap small
    No more buggy scrolling on small maps.

--- Topdown_Shooter/Assets/Scripts/CameraScrolling.cs
+++ Topdown_Shooter/Assets/Scripts/CameraScrolling.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 /// <summary>
-/// Currently, this code is strictly coupled with the sceneTilemap. Once the character goes to another screen, then the camera breaks.
-/// Only relevant on maps that are big enough.
+/// Camera follows player around while not veering offscreen.
+/// This code is strictly coupled with the sceneTilemap.
+/// 
 /// </summary>
 public class CameraScrolling : MonoBehaviour
 {
@@ -17,26 +18,94 @@ public class CameraScrolling : MonoBehaviour
 
     private float cameraWidth;
     private float cameraHeight;
-    private Vector3 minWorld;
-    private Vector3 maxWorld;
 
+    private Vector3 minSceneTilemapBounds;
+    private Vector3 maxSceneTilemapBounds;
+
+    // Max and min bounds of the camera
+    private float cameraMinX;
+    private float cameraMinY;
+    private float cameraMaxX;
+    private float cameraMaxY;
+    /// <summary>
+    /// Gets map and camera dimensions
+    /// Disables if there's no player, tilemap, or if the map too small. 
+    /// </summary>
     void Start()
     {
-        // Trim off Empty Tiles From Edges
-        sceneTilemap.CompressBounds();
+        if (playerTransform is null || sceneTilemap is null) 
+        { 
+            enabled = false; 
+        }
+        else
+        {
+            // Trim off Empty Tiles From Edges
+            sceneTilemap.CompressBounds();
 
-        // Define Camera Dimensions
-        Vector3 cameraDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-        cameraWidth= cameraDimensions.x;
-        cameraHeight= cameraDimensions.y;
+            // Define Camera Dimensions
+            Vector3 cameraDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+            cameraWidth = cameraDimensions.x;
+            cameraHeight = cameraDimensions.y;
 
-        // Define World Size
-        BoundsInt sceneTilemapBounds = sceneTilemap.cellBounds;
+            // Define TileMap Size
+            BoundsInt sceneTilemapBounds = sceneTilemap.cellBounds;
+            Vector3Int minCell = sceneTilemapBounds.min;
+            Vector3Int maxCell = sceneTilemapBounds.max;
+            minSceneTilemapBounds = sceneTilemap.CellToWorld(minCell);
+            maxSceneTilemapBounds = sceneTilemap.CellToWorld(maxCell);
 
-        Vector3Int minCell = sceneTilemapBounds.min;
-        Vector3Int maxCell = sceneTilemapBounds.max;
-        minWorld = sceneTilemap.CellToWorld(minCell);
-        maxWorld = sceneTilemap.CellToWorld(maxCell);
+            toggleCameraHorizonalVerticalMovements();
+        }
+    }
+    /// <summary>
+    /// Disable horizontal and/or vertical scrolling if map too small.
+    /// </summary>
+    private void toggleCameraHorizonalVerticalMovements()
+    {
+        //print($"MinWorld X: {minWorld.x} MaxWorld X: {maxWorld.x}");
+        //print($"MinWorld Y: {minWorld.y} MaxWorld Y: {maxWorld.y}");
+        //print($"CamWidth: {cameraWidth} CamHeight: {cameraHeight}");
+
+        float tilemapWidth = Mathf.Abs(minSceneTilemapBounds.x - maxSceneTilemapBounds.x) / 2;
+        float tilemapHeight = Mathf.Abs(minSceneTilemapBounds.y - maxSceneTilemapBounds.y) / 2;
+
+        if (cameraWidth >= tilemapWidth && cameraHeight >= tilemapHeight)
+        {
+            print("CAMERASCROLL: map too small, disabled");
+            enabled = false;
+        }
+        else
+        {
+            // Horizontal
+            if (cameraWidth < tilemapWidth)
+            {
+                // Enabled: Bounds of camera
+                cameraMinX = minSceneTilemapBounds.x + cameraWidth;
+                cameraMaxX = maxSceneTilemapBounds.x + cameraWidth;
+            }
+            else
+            {
+                print("CAMERASCROLL: world X too small. X scroll disabled");
+                float cameraStartPositionX = minSceneTilemapBounds.x + maxSceneTilemapBounds.x;
+                cameraMinX = cameraStartPositionX;
+                cameraMaxX = cameraStartPositionX;
+            }
+
+            // Vertical
+            if (cameraHeight < tilemapHeight)
+            {
+                // Bounds of camera
+                cameraMinY = minSceneTilemapBounds.y + cameraHeight;
+                cameraMaxY = maxSceneTilemapBounds.y + cameraHeight;
+            }
+            else
+            {
+                print("CAMERASCROLL: world Y too small. Y scroll disabled");
+                float cameraStartPositionY = minSceneTilemapBounds.y + maxSceneTilemapBounds.y;
+                cameraMinY = cameraStartPositionY;
+                cameraMaxY = cameraStartPositionY;
+            }
+        }
     }
 
     void Update()
@@ -48,19 +117,14 @@ public class CameraScrolling : MonoBehaviour
     /// </summary>
     private void CameraFollowPlayer()
     {
-        if (playerTransform != null)
-        {
-            float xOffset = cameraWidth;
-            float yOffset = cameraHeight;
             // Position is at player. But cannot go past bounds.
             Vector3 newCameraPosition = new Vector3(
-                Mathf.Clamp(playerTransform.position.x, minWorld.x + xOffset, maxWorld.x - xOffset),
-                Mathf.Clamp(playerTransform.position.y, minWorld.y + yOffset, maxWorld.y - yOffset),
+                Mathf.Clamp(playerTransform.position.x, cameraMinX, cameraMaxX),
+                Mathf.Clamp(playerTransform.position.y, cameraMinY, cameraMaxY),
                 transform.position.z
                 );
             //Vector3 newCameraPosition = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
 
             transform.position = newCameraPosition;
-        }
     }
 }
