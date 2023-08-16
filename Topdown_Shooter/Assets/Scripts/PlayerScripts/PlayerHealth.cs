42ac520b6a93a7789129b2943956c51621dc8db0
Chia Hung "Simon" Lee (he/him) <simonlee0423@gmail.com>
2023-08-16 11:50:18 -0400

    comments

--- Topdown_Shooter/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Topdown_Shooter/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -12,8 +12,7 @@ public class PlayerHealth : MonoBehaviour
 
     [SerializeField]
     private int invulnerabilityTime = 2000;
-    private bool playerInvulnerable;
-
+    private bool playerInvulnerable;    // So far this does nothing.
     private int enemyLayer;
 
     private void Awake()
@@ -22,7 +21,11 @@ public class PlayerHealth : MonoBehaviour
         enemyLayer = LayerMask.NameToLayer("Enemy");
         Health = MaxHealth;
     }
-
+    /// <summary>
+    /// For Taking damage, and Invulnerability.
+    /// I found this to be kinda clever, but if the collisionsstay is used for any logic other than taking damage, then I would need to take out the await in OnCollisionStay2D.
+    /// </summary>
+    /// <param name="collision"></param>
     private async void OnCollisionStay2D(Collision2D collision)
     {
         GameObject enemy = collision.gameObject;
@@ -33,7 +36,10 @@ public class PlayerHealth : MonoBehaviour
             await MakePlayerInvulnerable();
         }
     }
-
+    /// <summary>
+    /// Force the OnCollisionStay2D to wait.
+    /// </summary>
+    /// <returns></returns>
     private async Task MakePlayerInvulnerable()
     {
         playerInvulnerable= true;
@@ -42,7 +48,9 @@ public class PlayerHealth : MonoBehaviour
         playerInvulnerable = false;
         print("Player No Longer Invulnerable");
     }
-
+    /// <summary>
+    /// Draw Health to screen
+    /// </summary>
     void OnGUI()
     {
         GUILayout.BeginArea(new Rect(20, 20, 250, 120));
